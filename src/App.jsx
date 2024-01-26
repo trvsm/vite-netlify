401ea4165fca921bb4db799e34b6ee780de66a6b
Travis Martin <trvsmartin@gmail.com>
2024-01-26 13:52:48 -0800

    feat: setup folder structure, add about

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div><p>A Change</p>
+      <h1>Travis Martin's Blog</h1>
+      <p>
+        Welcome to my blog, where technology meets efficiency! I'll dive into
+        productivity with Python automation for daily tasks. Explore Odoo
+        development, from custom modules to JavaScript web services, as I learn
+        this open-source business suite. Finally, I'll touch on hosting large
+        language models and stable diffusion images on your Intel Mac, balancing
+        privacy, innovation, and budget. Welcome to this journey of empowerment
+        through code and technology!
+      </p>
+      {/* <div>
+        <p>A Change</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,9 +38,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
